ba1ea95cc141a31a50b88d882ba4b22f45b7a709
Vasily Ulianov <vasily@me.com>
2019-10-08 07:36:24 +0300

    Remove builder (will be unused)

--- Sources/PaymentUI/PaymentMethodsViewContoller.swift
+++ Sources/PaymentUI/PaymentMethodsViewContoller.swift
@@ -7,13 +7,14 @@ import Network
 	public weak var methodsTableView: UITableView!
 	public var listResultURL: URL
 	
-	let tableBuilder: PaymentMethodsTableViewBuilder
+	let configuration: PaymentMethodsTableViewConfiguration
+	let resultsController = PaymentMethodsTableViewResultsController()
 	var sessionStore: PaymentSessionStore?
 	
 	/// - Parameter tableConfiguration: settings for a payment table view, if not specified defaults will be used
 	/// - Parameter listResultURL: URL that you receive after executing *Create new payment session request* request. Needed URL will be specified in `links.self`
 	public init(tableConfiguration: PaymentMethodsTableViewConfiguration = DefaultPaymentMethodsTableViewConfiguration(), listResultURL: URL) {
-		self.tableBuilder = PaymentMethodsTableViewBuilder(configuration: tableConfiguration)
+		self.configuration = tableConfiguration
 		self.listResultURL = listResultURL
 		super.init(nibName: nil, bundle: nil)
 	}
@@ -26,18 +27,42 @@ import Network
 	override public func viewDidLoad() {
 		super.viewDidLoad()
 		
-		methodsTableView = tableBuilder.build()
+	}
+	
+	private func load(listResult: URL) {
+		let store = PaymentSessionStore(paymentSessionURL: listResult)
+		self.sessionStore = store
+		store.$session.subscribe { [weak resultsController] sessionState in
+			switch sessionState {
+			case .success(let session):
+				let group = PaymentMethodsTableViewResultsController.TableGroup(groupName: "Choose a method (localization required")
+				group.networks = session.networks
+				resultsController?.dataSource = [group]
+				fallthrough
+			default: dump(sessionState)
+			}
+		}
+		store.loadPaymentSession()
+	}
+	
+	// MARK: - Table View
+	
+	private func createMethodsTableView() -> UITableView {
+		let methodsTableView = UITableView(frame: CGRect.zero, style: .plain)
+		
+		configuration.customize?(tableView: methodsTableView)
+		
 		methodsTableView.translatesAutoresizingMaskIntoConstraints = false
+		methodsTableView.register(PaymentMethodsTableViewCell.self)
+		methodsTableView.dataSource = resultsController
 		view.addSubview(methodsTableView)
-		layoutMethodsTableView()
+		layoutMethodsTableView(methodsTableView)
+		resultsController.tableView = methodsTableView
+		
+		return methodsTableView
 	}
 	
-	public override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(animated)
-		tableBuilder.load(listResult: listResultURL)
-	}
-	
-	func layoutMethodsTableView() {
+	private func layoutMethodsTableView(_ methodsTableView: UITableView) {
 		NSLayoutConstraint.activate([
 			methodsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
 			methodsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -54,8 +79,6 @@ import Network
 				methodsTableView.topAnchor.constraint(equalTo: view.topAnchor)
 			])
 		}
-		
-		
 	}
 }
 
